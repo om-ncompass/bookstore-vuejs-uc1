7c59d24ddf94781dc9c7363c695e3e91dc49eadf
om-ncompass <omsatpathy@ncompass.inc>
2024-05-20 12:05:01 +0530

    Testing commit

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@
 
 <template>
   <h2>HELLO HUMAN</h2>
+  <div>Hello Huamn. Human is Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti pariatur obcaecati, eaque
+    error corporis quis deserunt atque sit voluptatibus et, corrupti unde! Incidunt fuga reiciendis in rem ad architecto
+    commodi.</div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
